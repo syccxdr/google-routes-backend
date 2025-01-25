20ef586d47a0eee26e242044d631999da635563c
suyingc <1070211640@qq.com>
2025-01-25 20:39:30 +0800

    feat: 添加csv依赖解析站点csv文件；文件有遗漏

--- src/main/java/com/example/google_backend/service/EtaService.java
+++ src/main/java/com/example/google_backend/service/EtaService.java
@@ -7,7 +7,7 @@ public interface EtaService {
     /**
      * Retrieves the estimated time of arrival for a bus at a given stop.
      *
-     * @param stopId The ID of the bus stop.
+     * @param stopName The Name of the bus stop.
      * @param routeId The ID of the bus route.
      * @param company The bus company.
      * @return A list of estimated times of arrival.
